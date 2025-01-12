bad4d21836d728a57189fd8b4b9ad936731cd26b
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-01-12 19:14:19 +0530

    Create closure_Scope.js

---
+++ closure_Scope.js
@@ -0,0 +1,41 @@
+function one(){
+    const username ="shilpi"
+    
+    function two(){
+        const website ="Youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two();
+}
+one()
+
+//with if-else
+if (true){
+    const username="shilpi"
+    
+    if(username==="shilpi"){
+        const website=" youtube";
+        console.log(username + website);
+    }
+    //console.log(website)
+        
+        
+    }
+    console.log(usernasme)
+
+
+//***************Interesting************//
+//js variable holding function called expression
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+//Not allowed
+
+
+addTwo(2)
+const addTwo = function(num){
+    return num+2
+}
