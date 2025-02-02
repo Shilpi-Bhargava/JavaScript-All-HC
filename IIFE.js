826997fc78457d15dfd4ce4f6e31bae6b2786554
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-02 22:45:37 +0530

    Create IIFE.js

---
+++ IIFE.js
@@ -0,0 +1,15 @@
+//
+( function chai(){
+  //named iife
+  console.log(`Db connected`);
+})();
+
+//arrowfunction
+(() =>{
+  console.log(`DB connected 2`);
+})();
+
+// passing 
+((name) =>{
+  cosnole.log(`passig ${name}`);
+}) ("Shilpi);
