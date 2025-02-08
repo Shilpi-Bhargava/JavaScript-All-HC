ec076ba4114dee3a56e606d11a9178e2211ecddb
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-08 17:53:41 +0530

    Update truthyFalsi.js

--- truthyFalsi.js
+++ truthyFalsi.js
@@ -4,3 +4,21 @@ if(userEmail){
 } else {
     console.log("falsy value")
 }
+
+//falsy value
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// truthy value
+//"0", 'false',"  ",  [], {}, function(){}
+
+//for run array
+const userName = []
+
+if (userEmail.length ===0){
+    console.log("array is empty")
+}
+//empty obj check
+const emptyobj = {}
+
+if (Object.keys(emptyobj).length === 0){
+console.log("Object is empty")
+}
