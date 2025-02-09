843537ae9c010cf56e693930497540211bc6ccb8
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-09 20:56:41 +0530

    Create loop2.js

---
+++ loop2.js
@@ -0,0 +1,21 @@
+//break and continue
+for (let i=1 ; i<=20: i++){
+  console.log(`Value of i is ${index}`)
+}
+for (let i=1 ; i<=20; i++){
+  
+  if(i==5){
+      console.log("value")
+      break
+      }
+      console.log(`Value of i is ${i}`)
+}
+
+for (let i=1 ; i<=20; i++){
+  
+  if(i==5){
+      console.log("value")
+      continue
+      }
+      console.log(`Value of i is ${i}`)
+}
