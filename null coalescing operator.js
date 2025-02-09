d8fa17a993991456d14daa93ed565c8acbd64b03
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-09 18:06:46 +0530

    Create null coalescing operator.js

---
+++ null coalescing operator.js
@@ -0,0 +1,9 @@
+//??null undefined\\
+val1=5 ?? 10
+val1 = null ?? 10
+val1 = null ?? 10 ??20
+val1 = undefimed ?? 15
+
+//Terniary operator
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("Less than 80") : console.log("more than 80")
