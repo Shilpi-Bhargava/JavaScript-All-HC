1cc5243ad5dede87ca11dd022364930a107ba4e2
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-13 22:41:55 +0530

    Create forEach2.js

---
+++ forEach2.js
@@ -0,0 +1,18 @@
+const coding =[
+    {
+  languageName: "javascript",
+  langFileName: "js"
+},
+{ languageName:"CPP",
+   langFileNAme: "CPP"
+},
+
+{
+    languageName: "java",
+    langfile: "jss"
+}
+
+]
+coding.forEach( (items) =>{
+    console.log(items.languageName)
+})
