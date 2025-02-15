ae0ed3ff6e85d5eba26544f8b520dc9c2c258f8a
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-15 17:34:03 +0530

    Create Filter2.js

---
+++ Filter2.js
@@ -0,0 +1,20 @@
+const books = [
+{title: 'Book1', genre: 'Fiction', publish: 1981, edition: 2004
+},
+{title: 'Book2', genre: 'History', publish: 1981, edition: 2004
+},
+{title: 'Book3', genre: 'History', publish: 1981, edition: 2004
+},
+{title: 'Book4', genre: 'Fiction', publish: 1981, edition: 2004
+},
+{title: 'Book5', genre: 'Science', publish: 1981, edition: 2004
+},
+{title: 'Book6', genre: 'Non-Fiction', publish: 1981, edition: 2004
+},
+{title: 'Book7', genre: 'Science', publish: 1981, edition: 2004
+},
+{title: 'Book8', genre: 'Non-Fiction', publish: 1981, edition: 2004
+},
+{title: 'Book9', genre: 'Romance', publish: 1981, edition: 2004
+}
+];
