40f3139133e69cd9ccf739ae4631e84f376103a7
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-15 15:57:36 +0530

    Update FilterMapReduce.js

--- FilterMapReduce.js
+++ FilterMapReduce.js
@@ -5,3 +5,26 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 }]
 console.log(`${myNums} and  filtered array is ${NewNum)`)
 
+// const coding =[ "js", "ruby", "java", "python", "cpp"]
+// const values= coding.forEach (  (val) =>{
+//   console.log(val)
+//   return val
+// }) not valid 
+
+ const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+ const NewNums = myNums.filter ( (num)  =>{
+  return num>4}
+  
+)
+console.log(NewNums)
+
+///Same with for Each
+
+const newNum =[]
+myNums.forEach( (num) =>{
+  if(num>4){
+    newNum.push(num)
+  }
+})
+console.log(newNum)
