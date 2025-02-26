68f30b6e1a6ae6c59b86f007200735776727b49a
Shilpi Bhargava <56435803+Shilpi-Bhargava@users.noreply.github.com>
2025-02-26 19:40:56 +0530

    Create Dom2.js

---
+++ Dom2.js
@@ -0,0 +1,6 @@
+document.querySelector('h2')
+document.querySelector('#title')
+document.querySelector('.heading')
+document.querySelector('input[type="passowrd"]')
+document.querySelcetor('p:first')
+  
